046dfd6772c7df9f178ae86ebf5ae111e368f310
Thadchet kittichotikul <42507576+Thadchet@users.noreply.github.com>
2020-07-29 17:02:30 +0700

    first commit

---
+++ wongnai/function.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import StaleElementReferenceException
+import pandas as pd
+import requests
+import time
+
